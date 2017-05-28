72a6f7995639739c4c787bc6150ff6123f108d13
unai.esteibar <unai.esteibar@xing.com>
2017-05-28 20:29:44 +0200

    feat(config): Allow to set url globally or for current process
    Fixes #2

--- test/neuron/config_test.exs
+++ test/neuron/config_test.exs
@@ -2,4 +2,34 @@ defmodule Neuron.ConfigTest do
   use ExUnit.Case
   doctest Neuron.Config
 
+  alias Neuron.Config
+
+  setup do
+    Config.set(:global, nil)
+    :ok
+  end
+
+  describe "set" do
+    test "global settings" do
+      url = "https://example.com/graph"
+      Config.set(:global, url: url)
+
+      assert Config.current_context() == :global
+      assert Config.get(:url) == url
+    end
+
+    test "process settings" do
+      url = "https://example.com/graph"
+
+      spawn(fn ->
+        Config.set(:process, url: url)
+
+        assert Config.current_context() == :process
+        assert Config.get(:url) == url
+      end)
+
+      assert Config.current_context() == :global
+      assert Config.get(:url) == nil
+    end
+  end
 end
