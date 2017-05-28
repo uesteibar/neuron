3843326af1693478c5e36442e6a1cef1e41394ee
unai.esteibar <unai.esteibar@xing.com>
2017-05-28 20:29:44 +0200

    feat(config): Allow to set url globally or for current process
    Fixes #2

--- lib/neuron/config.ex
+++ lib/neuron/config.ex
@@ -4,13 +4,21 @@ defmodule Neuron.Config do
   """
 
   @doc """
-  sets global configuration values for Neuron
+  sets global/process configuration values for Neuron
 
       iex> Neuron.Config.set(url: "http://example.com/graph")
       :ok
+
+      iex> Neuron.Config.set(:process, url: "http://example.com/graph")
+      :ok
   """
-  def set(value) do
+  def set(value), do: set(:global, value)
+  def set(:global, value) do
     Application.put_env(:neuron, :graphql, value)
+  end
+  def set(:process, value) do
+    Process.put(:neuron_graphql, value)
+    :ok
   end
 
   @doc """
@@ -20,9 +28,16 @@ defmodule Neuron.Config do
       "http://example.com/graph"
   """
   def get(:url) do
-    case Application.get_env(:neuron, :graphql, nil) do
+    case get_config_for(current_context()) do
       [url: url] -> url
       _ -> nil
     end
+  end
+
+  defp get_config_for(:process), do: Process.get(:neuron_graphql, nil)
+  defp get_config_for(:global), do: Application.get_env(:neuron, :graphql, nil)
+
+  def current_context() do
+    if Process.get(:neuron_graphql, nil), do: :process, else: :global
   end
 end
