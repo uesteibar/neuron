7f65a61436483830288c005ecc9843b31b6e1c68
Jered Masters <jeredmasters@users.noreply.github.com>
2018-02-27 09:20:50 +0800

    feat(deps): Upgrade httpoison to version 1.0

--- mix.exs
+++ mix.exs
@@ -27,7 +27,7 @@ defmodule Neuron.Mixfile do
 
   defp deps do
     [
-      {:httpoison, "~> 0.11"},
+      {:httpoison, "~> 1.0"},
       {:poison, "~> 3.1"},
       {:mock, "~> 0.3.1", only: :test},
       {:coverex, "~> 1.4", only: :test},
