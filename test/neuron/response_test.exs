ce2534e2d20785a89b3dbe23655c7674f0c6534a
sashman <sashman90@gmail.com>
2018-03-13 17:57:58 +0000

    fix(response): return `errors` alongside `data` (#11)
    When the GraphQL throws an error and then responds with 200, it sends the errors in `errors` alongside `data`.  For example, an unauthenticated request might have this response:
    
    ```
    {
      "data": null,
      "errors": [
        {
          "message": "Not authenticated",
          "locations": [
            {
              "line": 2,
              "column": 3
            }
          ],
          "path": [
            "..."
          ]
        }
      ]
    }
    ```
    
    BREAKING CHANGE
    
    Now the value of `response.body` will be `%{ data: ..., errors: ... }` instead of `%{ ... }` (only returning `data`).

--- test/neuron/response_test.exs
+++ test/neuron/response_test.exs
@@ -29,7 +29,27 @@ defmodule Neuron.ResponseTest do
       result = Response.handle({:ok, response})
 
       expected_result = %Response{
-        body: %{"users" => []},
+        body: %{"data" => %{"users" => []}},
+        headers: response.headers,
+        status_code: response.status_code
+      }
+
+      assert result == {:ok, expected_result}
+    end
+  end
+
+  describe "when successful response but with errors in body" do
+    setup do
+      %{
+        response: build_response(200, ~s/{"data": null, "errors": "stuff"}/)
+      }
+    end
+
+    test "returns the parsed Response struct", %{response: response} do
+      result = Response.handle({:ok, response})
+
+      expected_result = %Response{
+        body: %{"data" => nil, "errors" => "stuff"},
         headers: response.headers,
         status_code: response.status_code
       }
