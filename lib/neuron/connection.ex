6ba4b39d1ecd04bc4557ce231beeadfe640966f8
uesteibar <uesteibar@gmail.com>
2018-09-02 10:08:12 +0200

    feat(options): Allow to override options per request
    Fixes #18

--- lib/neuron/connection.ex
+++ lib/neuron/connection.ex
@@ -6,29 +6,16 @@ defmodule Neuron.Connection do
     raise ArgumentError, message: "you need to supply an url"
   end
 
-  def post(url, query) do
+  def post(url, query, headers) do
     HTTPoison.post(
       url,
       query,
-      build_headers(),
+      headers,
       connection_opts()
     )
-  end
-
-  defp build_headers() do
-    Config.get(:as_json)
-    |> base_headers()
-    |> Keyword.merge(headers())
-  end
-
-  defp headers() do
-    Config.get(:headers) || []
   end
 
   defp connection_opts() do
     Config.get(:connection_opts) || []
   end
-
-  defp base_headers(true), do: []
-  defp base_headers(_), do: ["Content-Type": "application/graphql"]
 end
--- test/neuron/connection_test.exs
+++ test/neuron/connection_test.exs
@@ -11,62 +11,20 @@ defmodule Neuron.ConnectionTest do
       %{url: "http://www.example.com/graph", query: %{}}
     end
 
-    test "sets content-type as default header", %{url: url, query: query} do
-      with_mock HTTPoison,
-        post: fn _url, _query, _headers, _opts ->
-          %HTTPoison.Response{}
-        end do
-        Connection.post(url, query)
-        assert called(HTTPoison.post(url, query, ["Content-Type": "application/graphql"], []))
-      end
-    end
-
-    test "overwrites content type if supplied", %{url: url, query: query} do
-      with_mock HTTPoison,
-        post: fn _url, _query, _headers, _opts ->
-          %HTTPoison.Response{}
-        end do
-        Neuron.Config.set(headers: ["Content-Type": "application/json"])
-        Connection.post(url, query)
-        assert called(HTTPoison.post(url, query, ["Content-Type": "application/json"], []))
-      end
-    end
-
     test "with basic auth", %{url: url, query: query} do
       with_mock HTTPoison,
         post: fn _url, _query, _headers, _opts ->
           %HTTPoison.Response{}
         end do
-        Neuron.Config.set(headers: [hackney: [basic_auth: [user: "password"]]])
-        Connection.post(url, query)
+        Connection.post(url, query, hackney: [basic_auth: [user: "password"]])
 
         assert called(
                  HTTPoison.post(
                    url,
                    query,
                    [
-                     "Content-Type": "application/graphql",
                      hackney: [basic_auth: [user: "password"]]
                    ],
-                   []
-                 )
-               )
-      end
-    end
-
-    test "with custom headers", %{url: url, query: query} do
-      with_mock HTTPoison,
-        post: fn _url, _query, _headers, _opts ->
-          %HTTPoison.Response{}
-        end do
-        Neuron.Config.set(headers: ["X-CUSTOM": "value"])
-        Connection.post(url, query)
-
-        assert called(
-                 HTTPoison.post(
-                   url,
-                   query,
-                   ["Content-Type": "application/graphql", "X-CUSTOM": "value"],
                    []
                  )
                )
@@ -79,29 +37,9 @@ defmodule Neuron.ConnectionTest do
           %HTTPoison.Response{}
         end do
         Neuron.Config.set(connection_opts: [timeout: 50_000])
-        Connection.post(url, query)
+        Connection.post(url, query, [])
 
-        assert called(
-                 HTTPoison.post(
-                   url,
-                   query,
-                   ["Content-Type": "application/graphql"],
-                   timeout: 50_000
-                 )
-               )
-      end
-    end
-
-    test "with as_json: true", %{url: url, query: query} do
-      with_mock HTTPoison,
-        post: fn _url, _query, _headers, _opts ->
-          %HTTPoison.Response{}
-        end do
-        Neuron.Config.set(as_json: true)
-        Connection.post(url, query)
-        Neuron.Config.set(as_json: false)
-
-        assert called(HTTPoison.post(url, query, [], []))
+        assert called(HTTPoison.post(url, query, [], timeout: 50_000))
       end
     end
   end
