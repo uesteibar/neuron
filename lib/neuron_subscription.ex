801c0fd31e07d1075098a61e27b6afdef01295b5
Karlo Smid <karlo.smid@gmail.com>
2021-12-10 14:18:17 +0100

    feat(neuron_subscription.ex): support for subscriptions where transport is Phoenix Channel
    Implementation is based on abshinte_websocket library.
    
    fix #33

---
+++ lib/neuron_subscription.ex
@@ -0,0 +1,39 @@
+defmodule Neuron.Subscription do
+  use GenServer
+
+  def start_link(opts) do
+    GenServer.start_link(Neuron.Subscription, opts)
+  end
+
+  @impl :true
+  def init(opts) do
+    query = Keyword.fetch!(opts, :query)
+    name = Keyword.fetch!(opts, :name)
+    variables = Keyword.fetch!(opts, :variables)
+    callback = Keyword.fetch!(opts, :callback)
+
+    subscription_server_name = Module.concat([name, Caller, SubscriptionServer])
+
+    AbsintheWebSocket.SubscriptionServer.subscribe(
+      subscription_server_name,
+      name,
+      callback,
+      query,
+      variables
+    )
+    {:ok, opts}
+  end
+
+  def supervisor(subscriber: subscriber) do
+
+    {AbsintheWebSocket.Supervisor,
+     [
+       subscriber: subscriber,
+       url: subscriber.url,
+       token: nil,
+       base_name: subscriber,
+       async: true
+     ]}
+
+  end
+end
