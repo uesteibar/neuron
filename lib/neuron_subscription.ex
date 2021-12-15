2c1825c332bcd85a5caaf15aaca44a0f0a06234e
Karlo Smid <karlo.smid@gmail.com>
2021-12-15 15:13:11 +0100

    feat(neuron_subscription.ex): abshinthe subscriptions
    Abshinthe subscriptions implementation based on elixir library abshinthe websocket.
    
    #33

--- lib/neuron_subscription.ex
+++ lib/neuron_subscription.ex
@@ -1,39 +1,33 @@
 defmodule Neuron.Subscription do
   use GenServer
 
-  def start_link(opts) do
-    GenServer.start_link(Neuron.Subscription, opts)
-  end
-
-  @impl :true
+  @impl true
   def init(opts) do
-    query = Keyword.fetch!(opts, :query)
-    name = Keyword.fetch!(opts, :name)
-    variables = Keyword.fetch!(opts, :variables)
-    callback = Keyword.fetch!(opts, :callback)
-
-    subscription_server_name = Module.concat([name, Caller, SubscriptionServer])
-
-    AbsintheWebSocket.SubscriptionServer.subscribe(
-      subscription_server_name,
-      name,
-      callback,
-      query,
-      variables
-    )
     {:ok, opts}
   end
 
-  def supervisor(subscriber: subscriber, url: url) do
-
+  def supervisor(subscriber: subscriber, url: url, token: token) do
     {AbsintheWebSocket.Supervisor,
      [
        subscriber: subscriber,
        url: url,
-       token: nil,
+       token: token,
        base_name: subscriber,
        async: true
      ]}
+  end
 
+  def subscribe(module, query) do
+    callback = fn result ->
+      apply(module, :handle_update, [result])
+    end
+
+    AbsintheWebSocket.SubscriptionServer.subscribe(
+      Module.concat(module, SubscriptionServer),
+      Neuron.Subscription,
+      callback,
+      query,
+      []
+    )
   end
 end
